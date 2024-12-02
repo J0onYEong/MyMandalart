ae64763e790f2b2efc4e3274940ae7bd4782a626
J0onYEong <cjy2134@naver.com>
2024-12-03 00:19:46 +0900

    Feat, Project+Environment 구현

---
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Env.swift
@@ -0,0 +1,15 @@
+import Foundation
+import ProjectDescription
+
+
+public extension Project {
+    
+    enum Environment {
+        
+        public static let appName: String = "MyMandarat"
+        public static let deploymentTarget: DeploymentTargets = .iOS("17.0")
+        public static let bundlePrefix: String = "com.choijunyeong.mymandarat"
+        
+    }
+}
+
