93face8596d632c0361d15a37ba8b8e36e1c75aa
J0onYEong <cjy2134@naver.com>
2024-12-24 20:43:01 +0900

    Feat, AcheivementRateView구현

---
+++ Projects/Feature/Home/Feature/Sources/View/SubMandarat/SubMandaratDisplayView/AcheivementRateView.swift
@@ -0,0 +1,118 @@
+//
+//  AcheivemnetRateView.swift
+//  Home
+//
+//  Created by choijunios on 12/24/24.
+//
+
+import UIKit
+
+import SharedCore
+
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class AcheivementRateView: UIView {
+    
+    // Sub view
+    private let rateStick: UIView = .init()
+    
+    
+    // Reactive
+    fileprivate let percentPublisher: BehaviorRelay<CGFloat> = .init(value: 0.0)
+    fileprivate let colorPublisher: PublishRelay<UIColor> = .init()
+    private let disposBag: DisposeBag = .init()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setUI()
+        setLayout()
+        setReative()
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let percent = percentPublisher.value
+        
+        let widthOfView = layer.bounds.width
+        let heightOfView = layer.bounds.height
+        let padding: CGFloat = 2
+        
+        let stickOrigin: CGPoint = .init(x: 2, y: 2)
+        let stickSize: CGSize = .init(
+            width: (widthOfView-2*padding) * percent,
+            height: heightOfView-2*padding
+        )
+        
+        rateStick.frame = .init(origin: stickOrigin, size: stickSize)
+    }
+    
+    private func setUI() {
+        
+        layer.cornerRadius = 5
+        layer.borderWidth = 1
+    }
+    
+    
+    private func setLayout() {
+        
+        // rateStick
+        addSubview(rateStick)
+    }
+    
+    
+    private func setReative() {
+        
+        // percent
+        percentPublisher
+            .observe(on: MainScheduler.asyncInstance)
+            .withUnretained(self)
+            .subscribe(onNext: { view, _ in
+                view.setNeedsLayout()
+            })
+            .disposed(by: disposBag)
+        
+        
+        colorPublisher
+            .observe(on: MainScheduler.asyncInstance)
+            .withUnretained(self)
+            .subscribe(onNext: { view, color in
+                
+                let colors = color.getTriadicColors()
+                let mainColor = color
+                let firstColor = colors[0]
+                let secondColor = colors[1]
+                
+                // #1. background
+                view.backgroundColor = firstColor
+                
+                // #2. border
+                view.layer.borderColor = secondColor.cgColor
+                
+                // #3. stick color
+                view.rateStick.backgroundColor = color
+                
+            })
+            .disposed(by: disposBag)
+    }
+}
+
+extension Reactive where Base == AcheivementRateView {
+    
+    var color: PublishRelay<UIColor> {
+        base.colorPublisher
+    }
+    
+    var percent: BehaviorRelay<CGFloat> {
+        base.percentPublisher
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 30, height: 10), body: {
+    
+    AcheivementRateView()
+})
---
+++ Projects/Feature/Home/Feature/Sources/View/SubMandarat/SubMandaratDisplayView/SubMandaratDisplayView.swift
@@ -0,0 +1,19 @@
+//
+//  SubMandaratDisplayView.swift
+//  Home
+//
+//  Created by choijunios on 12/24/24.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+class SubMandaratDisplayView: UIView {
+    
+    // Sub view
+    private let titleLabel: UILabel = .init()
+    
+}
