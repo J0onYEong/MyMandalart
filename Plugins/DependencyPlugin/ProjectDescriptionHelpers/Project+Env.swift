31bad20532fdd235f2ea5a5fd3ed51a6e4bd8efd
J0onYEong <cjy2134@naver.com>
2025-02-28 18:34:51 +0900

    Feat, iOS버전 수정

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Env.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Env.swift
@@ -7,7 +7,7 @@ public extension Project {
     enum Environment {
         
         public static let appName: String = "마이만다라트"
-        public static let deploymentTarget: DeploymentTargets = .iOS("18.0")
+        public static let deploymentTarget: DeploymentTargets = .iOS("17.5")
         public static let bundlePrefix: String = "com.choijunyeong.mymandalart"
         
     }
